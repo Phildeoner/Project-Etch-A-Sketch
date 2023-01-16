62c7d7c5a4b520202494840543f5084e0d321d9f
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2023-01-16 10:47:20 +0100

    Update all project documents and create generateGrid function

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
+//Set button to capture grid size
+const gridSizeBtn = document.getElementById("gridSizeBtn");
+gridSizeBtn.addEventListener("click", generateGrid);
+
+//A generateGrid function to accept input for grid size
+function generateGrid() {
+  let gridSize = prompt("Input Your Desired Grid Size:");
+  gridSize = parseInt(gridSize);
+
+//To capture the grid-container id in html div
+const gridContainer = document.getElementById("grid-container");
 //Create a loop to create grids
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
       let square = document.createElement("div");
-      square.className = "j-grid"
-      square.style.width = "30px";
-      square.style.height = "30px";
-      square.style.backgroundColor = "white";
-      square.style.border = "1px solid black";
-      square.style.display = "inline-block";
-      document.getElementById("grid-container").appendChild(square);
+      square.classList.add('gridBoxes');
+      gridContainer.appendChild(square);
     }
-    let newLine = document.createElement("br");
-    document.getElementById("grid-container").appendChild(newLine);
+  } 
   }
+
+  //Create an hover
+  let hoverEffect = document.querySelector(".j-grid");
+   hoverEffect.addEventListener("mouseover", 
+    function(){
+      hoverEffect.classList.add("effect");
+    });
+  hoverEffect.addEventListener("mouseout", 
+    function(){
+      hoverEffect.classList.remove("effect");
+    });
+
+
+//A function to  to draw a pixelated trail on the canvas
+let canvas = createCanvas(100,100);
+canvas.mousePressed(drawPixel);
+
+function drawPixel() {
+  strokeWeight(4);
+  point(mouseX, mouseY);
+}
   
